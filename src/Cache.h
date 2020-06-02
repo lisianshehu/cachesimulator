b40eb63ff0ab69c18e08775877ad1fc6756ee344
lisianshehu <46857154+lisianshehu@users.noreply.github.com>
2020-06-02 13:21:53 -0400

    Adding cache related data members and struct for object compostion

--- src/Cache.h
+++ src/Cache.h
@@ -6,18 +6,32 @@ class Cache
     public:
         int num_blocks;
         int associativity;
+        int replacementPolicy;
+        int writePolicy;
         unsigned long long int **cache;
         int **metaData;
         int **writeMetaData;
+
+        struct
+        {
+            bool hit;
+            int totalHits;
+            int indexHit;
+            int readsMem;
+            int writesMem;
+            int totalMisses;
+        } cache_data;
 
         void initialize_cache();
         void initialize_metaData();
         void initialize_writeMetaData();
 
-    Cache(int num_blocks, int associativity)
+    Cache(int num_blocks, int associativity, int replacementPolicy, int writePolicy)
     {
         this->num_blocks = num_blocks;
         this->associativity = associativity;
+        this->replacementPolicy = replacementPolicy;
+        this->writePolicy = writePolicy;
     }
    
 };
