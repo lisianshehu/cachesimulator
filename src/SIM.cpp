921858d48188c3210e21069959cbd0cfbb0c3915
lisianshehu <46857154+lisianshehu@users.noreply.github.com>
2020-06-02 19:52:42 -0400

    removing commented lines

--- src/SIM.cpp
+++ src/SIM.cpp
@@ -25,11 +25,7 @@ int main(int argc, char** argv)
     string tracefilepath = argv[5];
 
     double totalAccesses = 0;
-    double totalMisses = 0;
     double missRatio = 0.0;
-    int maxPosition  = 0;
-    bool fullSet = false;
-    bool setFull = false;
     string operation;
     string rawByteHexAddress;
    
@@ -62,67 +58,11 @@ int main(int argc, char** argv)
             perform_write(cache, operation, setNumber, tagAddress);
 
             // reset variables
-            setFull = false; 
             cache->cache_data.hit = false;
         }
         else if (operation == "R") // read operation
         {   
             perform_read(cache, operation, setNumber, tagAddress);
-
-        //     for (int i = 0; i < associativity; i++)
-        //     {
-        //         if (cache.cache[setNumber][i] == tagAddress)
-        //         {
-        //             hit = true;
-        //             totalHits++;
-        //             indexHit = i;
-        //             break;
-        //         } 
-        //     }
-        //     // update metadata for a hit
-        //     if (hit)
-        //     {
-        //         if(replacementPolicy == 0)
-        //             updateMetaData(indexHit, setNumber, numberOfBlocks, associativity, cache.cache, cache.metaData);            
-        //     }
-        //     else if (!hit) // perform cache miss process
-        //     {
-        //         bool fullSet;
-        //         readsMem++;
-        //         totalMisses++;
-        //         if (!setFull)
-        //         {
-        //             if (replacementPolicy == 0) // LRU replacement
-        //                 fullSet = insertIntoFreeCacheLRU(operation, writePolicy, setFull, setNumber, numberOfBlocks, associativity, tagAddress, cache.cache, cache.metaData, cache.writeMetaData);   
-
-        //             if (replacementPolicy == 1) // FIFO replacement
-        //                 fullSet = insertIntoFreeCacheFIFO(operation, writePolicy, setFull, setNumber, numberOfBlocks, associativity, tagAddress, cache.cache, cache.metaData, cache.writeMetaData);
-        //         }
-
-        //         // checks for empty sets to place data and sets metadata to MRU data and decide which element to evict
-        //         if (fullSet)
-        //         {
-        //             int writeBack = 0;
-
-        //             // LRU replacement
-        //             if (replacementPolicy == 0) 
-        //                 writeBack = insertIntoFullCacheLRU(operation, writePolicy, writesMem, setNumber, numberOfBlocks, associativity, maxPosition, tagAddress, cache.cache, cache.metaData, cache.writeMetaData);
-                    
-        //             // FIFO replacement
-        //             if (replacementPolicy == 1) 
-        //                 writeBack = insertIntoFullCacheFIFO(operation, writePolicy, writesMem, setNumber, numberOfBlocks, associativity, maxPosition, tagAddress, cache.cache, cache.metaData, cache.writeMetaData);
-                    
-        //             // writes memory if a write back was triggered
-        //             if (writePolicy == 1 && writeBack == 1) 
-        //             {
-        //                 writesMem++;
-        //             }
-        //         }
-        //     }
-        //     // reset variables
-        //     setFull = false;
-        //     maxPosition = 0;
-        //     hit = false;
         }
     }
 
