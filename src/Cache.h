1c0057f9f7f8039da577876b994e90725e2f2ab9
lisianshehu <46857154+lisianshehu@users.noreply.github.com>
2020-06-08 20:32:33 -0400

    Making Cache class abstract with virtual functions to be implemented in child classes

--- src/Cache.h
+++ src/Cache.h
@@ -1,38 +1,42 @@
 #ifndef CACHE_H
 #define CACHE_H
+#include <string>
+using namespace std;
 
 class Cache
 {
     public:
         int num_blocks;
         int associativity;
-        int replacementPolicy;
-        int writePolicy;
+        int write_policy;
+
+        // data structures for cache
         unsigned long long int **cache;
         int **metaData;
         int **writeMetaData;
 
         struct CacheData
         {
-            bool hit;
-            int totalHits;
-            int indexHit;
-            int readsMem;
-            int writesMem;
-            int totalMisses;
-        } cache_data;
+            bool hit = false;
+            int indexHit = 0;
+            int readsMem = 0;
+            int writesMem = 0;
+            int totalMisses = 0;
+        }cache_data;
 
         void initialize_cache();
         void initialize_metaData();
         void initialize_writeMetaData();
 
-    Cache(int num_blocks, int associativity, int replacementPolicy, int writePolicy)
+        virtual bool insert_into_free_cache(Cache *cache, std::string operation, int set_number, unsigned long long int tag_address) = 0;
+        virtual int insert_into_full_cache(Cache *cache, std::string operation, int set_number, unsigned long long int tag_address) = 0;
+        virtual void updateMetaData(Cache *cache, int set_number) = 0;
+
+    Cache(int num_blocks, int associativity, int write_policy)
     {
         this->num_blocks = num_blocks;
         this->associativity = associativity;
-        this->replacementPolicy = replacementPolicy;
-        this->writePolicy = writePolicy;
-    }
-   
+        this->write_policy = write_policy;
+    }   
 };
 #endif
