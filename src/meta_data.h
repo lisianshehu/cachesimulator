6468f8809bba67356728d8478b53e4dbea103859
lisianshehu <46857154+lisianshehu@users.noreply.github.com>
2020-06-08 20:37:04 -0400

    removed updateMetaData function which is to be moved to LRUCache class

--- src/meta_data.h
+++ src/meta_data.h
@@ -1,6 +1,5 @@
 #ifndef META_DATA_H
 #define META_DATA_H
 #include "Cache.h"
-extern void updateMetaData(Cache *cache, int set_number);
 extern int writeBackDataCheck(Cache *cache, int set_number, int replcIndex, unsigned long long int tag_address);
 #endif
