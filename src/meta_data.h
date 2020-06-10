654df4eb763d38cba3e575220c007bdfa48a1ef0
lisianshehu <46857154+lisianshehu@users.noreply.github.com>
2020-06-10 15:11:13 -0400

    deleted meta_data related files as it was consolidated into different header files

--- src/meta_data.h
+++
@@ -1,5 +0,0 @@
-#ifndef META_DATA_H
-#define META_DATA_H
-#include "Cache.h"
-extern int writeBackDataCheck(Cache *cache, int set_number, int replcIndex, unsigned long long int tag_address);
-#endif
